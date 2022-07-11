03bab0a8ec31402439d3fe6be2b84fd2282805d6
JitPackJoyride <ajitkrishna99@gmail.com>
2022-07-11 21:50:47 +0800

    feat: did zelt test

--- src/v2/user-enrichment.service.ts
+++ src/v2/user-enrichment.service.ts
@@ -1,12 +1,23 @@
-import { IntegrationService } from "./service/integration.service";
-import { UserAddressService } from "./service/user-address.service";
+import {ExternalEmployee, IntegrationService} from "./service/integration.service";
+import {UserAddress, UserAddressService} from "./service/user-address.service";
 import { UserBankDetailsService } from "./service/user-bank.service";
 import { UserService } from "./service/user.service";
+import {updateUser} from "../v1/service/users";
 
 export interface MatchedUser {
   userId: number;
   identifier: string;
   employeeID?: string | undefined;
+}
+
+interface IdentifiableExternalUser extends ExternalEmployee {
+  identifier: string
+  userId?: number
+}
+
+interface ExternalUserAddresses {
+  userId: number;
+  address: UserAddress
 }
 
 export class UserEnrichmentService {
@@ -18,7 +29,51 @@ export class UserEnrichmentService {
   ) {}
 
   // TODO implementation goes here
-  enrichUsers(companyId: number, matchedUsers: MatchedUser[]) {
-    throw new Error("Method not implemented.");
+  async enrichUsers(companyId: number, matchedUsers: MatchedUser[]) {
+  // Call integration service and retrieve the external users
+   const externalUsers = await this.integrationService.getExternalEmployees(companyId);
+   const identifiableExternalUsers: IdentifiableExternalUser[] = externalUsers.map(externalUser => {
+     return {
+       ...externalUser,
+       identifier: `${externalUser.FamilyName} ${externalUser.GivenName}`.toLowerCase()
+     }
+   })
+
+  // Match the local users with the external users
+  //  TODO: Fix name for this variable
+    const externalUsers2: IdentifiableExternalUser[] = identifiableExternalUsers.map(externalUser => {
+      const matchedUser = matchedUsers.find(user => user.identifier.toLowerCase() === externalUser.identifier);
+      if (!matchedUser) return externalUser;
+      return {
+        ...externalUser, userId: matchedUser.userId
+      }
+    })
+
+  //  For each external user, upsert the user information with address
+    let addressesToUpdate: ExternalUserAddresses[] = [];
+
+   externalUsers2.forEach((externalUser) => {
+     // If could not match user
+     if (!externalUser.userId) return;
+
+     //  Convert external user address format to database format
+     const {PrimaryAddr: externalAddress} = externalUser
+
+      const userAddress: UserAddress = {
+        streetName: externalAddress.StreetLine,
+        city: externalAddress.City,
+        country: externalAddress.Country
+      }
+
+      addressesToUpdate.push({
+        userId: externalUser.userId,
+        address: userAddress
+      })
+    })
+
+  //  Update all information of address
+    for (const addressToUpdate of addressesToUpdate) {
+      await this.userAddressService.updateAddress(addressToUpdate.userId, addressToUpdate.address)
+    }
   }
 }
